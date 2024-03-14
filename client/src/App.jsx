346936a5f52349ae6a382d4d5b5345c6292cb374
dohendra <bholubabu420@gmail.com>
2024-03-14 23:28:24 +0530

    tailwind bug resolved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,9 @@ import Home from './pages/Home';
 import About from './pages/About';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
-import Profile from './pages/Profile';
+// import Profile from './pages/Profile';
 import Header from './components/Header';
-import PrivateRoute from './components/PrivateRoute';
+// import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
   return (
@@ -17,10 +17,11 @@ export default function App() {
         <Route path='/about' element={<About />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
-        <Route element={<PrivateRoute />}>
+        {/* <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
-        </Route>
+        </Route> */}
       </Routes>
     </BrowserRouter>
   );
 }
+
